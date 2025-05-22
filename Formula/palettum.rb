399981dbdcecdb1bc3977b86e1c76b4cd78371d1
arrowpc <battle.beater2005@gmail.com>
2025-05-22 16:50:17 +1000

    Update to v0.5.2

--- Formula/palettum.rb
+++ Formula/palettum.rb
@@ -2,17 +2,17 @@ class Palettum < Formula
   desc "CLI tool that recolors images & GIFs with any palette"
   homepage "https://github.com/ArrowPC/palettum/"
   license "AGPL-3.0-only"
-  version "0.5.1"
+  version "0.5.2"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/arrowpc/palettum/releases/download/v0.5.1/palettum-macOS-x86_64.tar.gz"
-      sha256 "90eb5c2247211f6ef7013cdec4dba4cbce175dc0b5c02ca76e9499ec54a9e1cc"
+      url "https://github.com/arrowpc/palettum/releases/download/v0.5.2/palettum-macOS-x86_64.tar.gz"
+      sha256 "18cd88844063edd6157a84f282d2d1bc7de871c76cc007709898656d4efe453e"
     end
 
     if Hardware::CPU.arm?
-      url "https://github.com/arrowpc/palettum/releases/download/v0.5.1/palettum-macOS-arm64.tar.gz"
-      sha256 "2e926f74f8f9234200d1a077a7d0780d2f53c5c740a876b50e962fb18f799cfd"
+      url "https://github.com/arrowpc/palettum/releases/download/v0.5.2/palettum-macOS-arm64.tar.gz"
+      sha256 "4b9732ffea1110851a510bec30fd4adc5e9c532a76b5c7a57c66fcc7dc3c16eb"
     end
   end
 
