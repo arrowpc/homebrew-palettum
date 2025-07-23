912604e37f7a18926da0376683a764d84995b064
arrowpc <battle.beater2005@gmail.com>
2025-07-23 19:58:27 +1000

    Update to v0.6.1

--- Formula/palettum.rb
+++ Formula/palettum.rb
@@ -2,20 +2,20 @@ class Palettum < Formula
   desc "CLI tool that recolors images & GIFs with any palette"
   homepage "https://github.com/ArrowPC/palettum/"
   license "AGPL-3.0-only"
-  version "0.6.0"
+  version "0.6.1"
 
   depends_on "ffmpeg"
   depends_on "pkg-config"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/arrowpc/palettum/releases/download/v0.6.0/palettum-x86_64-apple-darwin-video-gpu.tar.gz"
-      sha256 "06de4038090e734cad202f07bdaff9d6a9b62fe37ccfdd363a7aecc11e66102e"
+      url "https://github.com/arrowpc/palettum/releases/download/v0.6.1/palettum-x86_64-apple-darwin-video-gpu.tar.gz"
+      sha256 "982a3117e2f802486de2a130491639211486854c2b077dbae592bb8d1dfa6b91"
     end
 
     if Hardware::CPU.arm?
-      url "https://github.com/arrowpc/palettum/releases/download/v0.6.0/palettum-aarch64-apple-darwin-video-gpu.tar.gz"
-      sha256 "982a3117e2f802486de2a130491639211486854c2b077dbae592bb8d1dfa6b91"
+      url "https://github.com/arrowpc/palettum/releases/download/v0.6.1/palettum-aarch64-apple-darwin-video-gpu.tar.gz"
+      sha256 "7e2b07790c74d46e3c780b8dd20e10ea728b38f7bffe879b39c535dac3619ea3"
     end
   end
 
